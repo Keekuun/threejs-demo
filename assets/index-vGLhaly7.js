41f3eb6c72f39c729ced55caaca01420adac8292
Keekuun <Keekuun@users.noreply.github.com>
2025-07-07 11:48:51 +0000

    deploy: d0e11b4531dbd60fd4ba040d771fc08413e63115

---
+++ assets/index-vGLhaly7.js
@@ -0,0 +1,1 @@
+import"./modulepreload-polyfill-B5Qt9EMX.js";/* empty css              */import{f as o}from"./three.module-BfP8F9u9.js";const e={BASE_URL:"/threejs-demo/",DEV:!1,MODE:"production",PROD:!0,SSR:!1};console.log(o);console.log("import.meta.env",e);
